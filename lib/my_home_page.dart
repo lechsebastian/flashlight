01cc49afd8f40a326d95c757d9131aff0d3be1c1
Sebastian Lech <lechsebastian99@gmail.com>
2025-04-23 19:29:25 +0200

    Improve UI app

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -8,8 +8,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isTurnedOn = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: isTurnedOn ? Colors.black : Colors.white,
+      ),
+      backgroundColor: isTurnedOn ? Colors.black : Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  isTurnedOn = !isTurnedOn;
+                });
+                Flashlight();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: isTurnedOn ? Colors.white : Colors.black,
+                    width: 2,
+                  ),
+                  color: isTurnedOn ? Colors.black : Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.power_settings_new,
+                  size: 120,
+                  color: isTurnedOn ? Colors.white : Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  
+  void Flashlight() {
+    
   }
 }
